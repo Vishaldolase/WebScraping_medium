8a7e0d7d0af19f8da27d5a93fd88989cde963d97
vishaldolase <vishaldolase@gmail.com>
2022-05-05 16:48:44 +0530

    deployment changes

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n@lf9h=7g+&-c5avxa8(42x45jf_n#0fe3avy!)c60!unixt^t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- scrapper/urls.py
+++ scrapper/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import include
+from django.conf.urls import include,url
 from django.urls import re_path
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^medium/', include('medium.urls')),
     re_path(r'^', include('medium.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
